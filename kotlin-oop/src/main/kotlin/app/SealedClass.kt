63c9ed40c51687dc70ee480788c6216351170490
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-01-05 21:47:19 +0700

    Sealed Class

---
+++ kotlin-oop/src/main/kotlin/app/SealedClass.kt
@@ -0,0 +1,18 @@
+package app
+
+import data.Minus
+import data.Modulo
+import data.Operation
+import data.Plus
+
+fun operation(value1 : Int, value2: Int, operation: Operation): Int {
+    return when (operation) {
+        is Plus -> value1 + value2
+        is Minus -> value1 - value2
+        is Modulo -> value1 % value2
+    }
+}
+
+fun main() {
+    println(operation(10, 5, Plus()))
+}
